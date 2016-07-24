fae70c66d5f5682ac5dfff61013d58b3026ea04e
Kamil Rytarowski <n54@gmx.com>
2016-07-24 13:39:27 +0200

    py-numpy: Add check for sys/endian.h

--- py-numpy/patches/patch-numpy_core_setup.py
+++ py-numpy/patches/patch-numpy_core_setup.py
@@ -1,2 +1,15 @@
-$NetBSD: patch-numpy_core_setup.py,v 1.2 2015/04/17 00:41:38 wen Exp $
-Do not require Atlas to make 'dotblas' module; libblas should be sufficient.
+$NetBSD$
+
+--- numpy/core/setup.py.orig	2016-07-24 11:35:48.946656208 +0000
++++ numpy/core/setup.py
+@@ -272,6 +272,10 @@ def check_types(config_cmd, ext, build_d
+     if res:
+         private_defines.append(('HAVE_ENDIAN_H', 1))
+         public_defines.append(('NPY_HAVE_ENDIAN_H', 1))
++    res = config_cmd.check_header("sys/endian.h")
++    if res:
++        private_defines.append(('HAVE_SYS_ENDIAN_H', 1))
++        public_defines.append(('NPY_HAVE_SYS_ENDIAN_H', 1))
+ 
+     # Check basic types sizes
+     for type in ('short', 'int', 'long'):
