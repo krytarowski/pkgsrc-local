c4c01b24a392cdb8087f20f8e388e98819384f39
Kamil Rytarowski <n54@gmx.com>
2016-07-27 10:57:53 +0200

    agm: Try to fix RCSIDs

--- agm/patches/patch-gobble.c
+++
@@ -1,6 +0,0 @@
-Cannot strip away RCS IDs, please handle manually!
-These are the matching lines:
-   -char gobble_RCSid[] = "$Revision: 1.4 $";
-   @@ -24,0 +25 @@ char gobble_RCSid[] = "$Revision: 1.4 $"
-Setting PKGDIFF_FMT might help.
-Otherwise you may need to run diff by hand.
