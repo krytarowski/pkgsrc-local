ad8ef0a2b7c5681a1f61e209fe78d5a1651c1bd4
Kamil Rytarowski <n54@gmx.com>
2016-07-27 11:03:13 +0200

    agm: FIxes

--- agm/patches/patch-gobble.c
+++ agm/patches/patch-gobble.c
@@ -1,18 +1,19 @@
 $NetBSD$
 
---- gobble.c.orig	2016-07-27 08:59:42.595830335 +0000
+--- gobble.c.orig	2016-07-27 09:01:17.572044212 +0000
 +++ gobble.c
-@@ -22,6 +22,9 @@ char gobble_RCSid[] = "Revision: 1.4 $";
+@@ -22,6 +22,10 @@ char gobble_RCSid[] = "Revision: 1.4 $";
  
  #include "agm.h"
  
 +#include <stdlib.h>
++#include <unistd.h>
 +
 +void
  gobble_file (filename, command)
      char *filename, *command;
  {
-@@ -81,4 +84,3 @@ gobble_file (filename, command)
+@@ -81,4 +85,3 @@ gobble_file (filename, command)
    }
    if (notquiet) printf (" %d words\n", wordcount - before_count);
  }
