20d05343278e9e26d3d6c188562261d4bec23d55
Kamil Rytarowski <n54@gmx.com>
2016-08-12 02:05:58 +0200

    openrw: Fixes

--- openrw/patches/patch-rwgame_GameConfig.cpp
+++ openrw/patches/patch-rwgame_GameConfig.cpp
@@ -1,13 +1,13 @@
 $NetBSD$
 
---- rwgame/GameConfig.cpp.orig	2016-08-11 22:19:42.000000000 +0000
+--- rwgame/GameConfig.cpp.orig	2016-08-12 00:03:52.000000000 +0000
 +++ rwgame/GameConfig.cpp
 @@ -43,7 +43,7 @@ bool GameConfig::isValid()
  
  std::string GameConfig::getDefaultConfigPath()
  {
--#if defined(RW_LINUX) || defined(RW_FREEBSD)
-+#if defined(RW_LINUX) || defined(RW_FREEBSD) || defined(RW_NETBSD)
+-#if defined(RW_LINUX) || defined(RW_FREEBSD) || defined(RW_OPENBSD)
++#if defined(RW_LINUX) || defined(RW_FREEBSD) || defined(RW_NETBSD) || defined(RW_OPENBSD)
  	char* config_home = getenv("XDG_CONFIG_HOME");
  	if (config_home != nullptr) {
  		return std::string(config_home) + "/" + kConfigDirectoryName;
