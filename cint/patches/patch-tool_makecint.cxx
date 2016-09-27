d9e19e201bd2d71c854933ce2ae676eb1df320fe
Kamil Rytarowski <n54@gmx.com>
2016-09-27 04:05:55 +0200

    cint: Fixes

---
+++ cint/patches/patch-tool_makecint.cxx
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- tool/makecint.cxx.orig	2007-03-16 11:32:07.000000000 +0000
++++ tool/makecint.cxx
+@@ -31,6 +31,7 @@
+ #endif
+ 
+ #include <string>
++#include <cstring>
+ #include <iostream>
+ #include <fstream>
+ #include <list>
