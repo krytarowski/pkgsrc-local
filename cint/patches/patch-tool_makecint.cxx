69253239ca5e6ae301d86f32152e43238050b753
Kamil Rytarowski <n54@gmx.com>
2016-09-27 04:23:50 +0200

    cint: Fixes

--- cint/patches/patch-tool_makecint.cxx
+++ cint/patches/patch-tool_makecint.cxx
@@ -2,9 +2,11 @@ $NetBSD$
 
 --- tool/makecint.cxx.orig	2007-03-16 11:32:07.000000000 +0000
 +++ tool/makecint.cxx
-@@ -31,6 +31,7 @@
+@@ -30,7 +30,9 @@
+ #define EXIT_SUCCESS 0
  #endif
  
++#include <cstdlib>
  #include <string>
 +#include <cstring>
  #include <iostream>
