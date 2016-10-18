a5369dad5f8a380c9bedd07a59bf64e2cc71cbdc
Kamil Rytarowski <n54@gmx.com>
2016-10-18 06:29:27 +0200

    opensubdiv: Fix doc option

--- opensubdiv/options.mk
+++ opensubdiv/options.mk
@@ -3,7 +3,7 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensubdiv
 PKG_SUPPORTED_OPTIONS=	opengl ptex doc # cuda tbb opencl dx11
 PKG_SUGGESTED_OPTIONS=	doc # ptex opengl
-PLIST_VARS+=		opengl # ptex has no extra PLIST entries
+PLIST_VARS+=		opengl doc # ptex has no extra PLIST entries
 
 .include 		"../../mk/bsd.options.mk"
 
@@ -25,7 +25,9 @@ CMAKE_ARGS+=	-DNO_PTEX:BOOL=ON
 .if !empty(PKG_OPTIONS:Mdoc)
 BUILD_DEPENDS+=	doxygen>=1.8.9.1:../../devel/doxygen
 BUILD_DEPENDS+=	${PYPKGPREFIX}-docutils-[0-9]*:../../textproc/py-docutils
+CMAKE_ARGS+=	-DRST2HTML_EXECUTABLE:PATH=${PREFIX}/bin/rst2html-${PYVERSSUFFIX}.py
 .include "../../lang/python/pyversion.mk"
+PLIST.doc=	yes
 .else
 CMAKE_ARGS+=	-DNO_DOC:BOOL=ON
 .endif
