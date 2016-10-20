16856d42f9e7792fa0509bb0167d47481fec8756
Kamil Rytarowski <n54@gmx.com>
2016-10-20 04:03:11 +0200

    fixes

--- threadingbuildingblocks/patches/patch-build_version__info__netbsd.sh
+++ threadingbuildingblocks/patches/patch-build_version__info__netbsd.sh
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- build/version_info_netbsd.sh.orig	2016-10-19 23:17:39.829676547 +0000
+--- build/version_info_netbsd.sh.orig	2016-10-20 01:46:08.880514594 +0000
 +++ build/version_info_netbsd.sh
-@@ -0,0 +1,29 @@
+@@ -0,0 +1,32 @@
 +#!/bin/sh
 +#
 +# Copyright (c) 2005-2016 Intel Corporation
@@ -25,9 +25,12 @@ $NetBSD$
 +
 +# Script used to generate version info string
 +echo "#define __TBB_VERSION_STRINGS(N) \\"
-+echo '#N": BUILD_HOST'"\t"`hostname`" ("`arch`")"'" ENDL \'
-+echo '#N": BUILD_OS'"\t\t"`uname`'" ENDL \'
-+echo '#N": BUILD_KERNEL'"\t"`uname -srv`'" ENDL \'
++echo '#N": BUILD_HOST'"\t\t"`hostname -s`" ("`uname -m`")"'" ENDL \'
++echo '#N": BUILD_OS'"\t\t"`uname -sr`'" ENDL \'
++echo '#N": BUILD_KERNEL'"\t"`uname -i`'" ENDL \'
++echo '#N": BUILD_GCC'"\t\t"`${CXX} -dumpversion`'" ENDL \'
++[ -z "$COMPILER_VERSION" ] || echo '#N": BUILD_COMPILER'"\t"$COMPILER_VERSION'" ENDL \'
++echo '#N": BUILD_LD'"\t\t"`ld -v 2>&1 | head -n 1`'" ENDL \'
 +echo '#N": BUILD_TARGET'"\t$arch on $runtime"'" ENDL \'
 +echo '#N": BUILD_COMMAND'"\t"$*'" ENDL \'
 +echo ""
