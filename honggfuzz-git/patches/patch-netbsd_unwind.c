aafaf75d277af4ae167aa3f960ff9597075358a8
Kamil Rytarowski <n54@gmx.com>
2018-08-10 00:30:26 +0200

    fix

--- honggfuzz-git/patches/patch-netbsd_unwind.c
+++ honggfuzz-git/patches/patch-netbsd_unwind.c
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- netbsd/unwind.c.orig	2018-08-09 17:30:27.962762052 +0000
+--- netbsd/unwind.c.orig	2018-08-09 21:49:50.951661439 +0000
 +++ netbsd/unwind.c
-@@ -0,0 +1,148 @@
+@@ -0,0 +1,77 @@
 +/*
 + *
 + * honggfuzz - architecture dependent code (NETBSD/UNWIND)
@@ -43,21 +43,6 @@ $NetBSD$
 + * http://www.nongnu.org/libunwind/man/libunwind-ptrace(3).html
 + */
 +
-+// libunwind error codes used for debugging
-+static const char* UNW_ER[] = {
-+    "UNW_ESUCCESS",     /* no error */
-+    "UNW_EUNSPEC",      /* unspecified (general) error */
-+    "UNW_ENOMEM",       /* out of memory */
-+    "UNW_EBADREG",      /* bad register number */
-+    "UNW_EREADONLYREG", /* attempt to write read-only register */
-+    "UNW_ESTOPUNWIND",  /* stop unwinding */
-+    "UNW_EINVALIDIP",   /* invalid IP */
-+    "UNW_EBADFRAME",    /* bad frame */
-+    "UNW_EINVAL",       /* unsupported operation or bad value */
-+    "UNW_EBADVERSION",  /* unwind info has unsupported version */
-+    "UNW_ENOINFO"       /* no unwind info found */
-+};
-+
 +typedef struct {
 +    unsigned long start;
 +    unsigned long end;
@@ -67,62 +52,6 @@ $NetBSD$
 +    unsigned long inode;
 +    char name[PATH_MAX];
 +} procMap_t;
-+
-+static procMap_t* arch_parsePidMaps(pid_t pid, size_t* mapsCount) {
-+    FILE* f = NULL;
-+    char fProcMaps[PATH_MAX] = {0};
-+    snprintf(fProcMaps, PATH_MAX, "/proc/%d/maps", pid);
-+
-+    if ((f = fopen(fProcMaps, "rb")) == NULL) {
-+        PLOG_E("Couldn't open '%s' - R/O mode", fProcMaps);
-+        return 0;
-+    }
-+    defer {
-+        fclose(f);
-+    };
-+
-+    *mapsCount = 0;
-+    procMap_t* mapsList = malloc(sizeof(procMap_t));
-+    if (mapsList == NULL) {
-+        PLOG_W("malloc(size='%zu')", sizeof(procMap_t));
-+        return NULL;
-+    }
-+
-+    while (!feof(f)) {
-+        char buf[sizeof(procMap_t) + 1];
-+        if (fgets(buf, sizeof(buf), f) == 0) {
-+            break;
-+        }
-+
-+        mapsList[*mapsCount].name[0] = '\0';
-+        sscanf(buf, "%lx-%lx %5s %lx %7s %ld %s", &mapsList[*mapsCount].start,
-+            &mapsList[*mapsCount].end, mapsList[*mapsCount].perms, &mapsList[*mapsCount].offset,
-+            mapsList[*mapsCount].dev, &mapsList[*mapsCount].inode, mapsList[*mapsCount].name);
-+
-+        *mapsCount += 1;
-+        if ((mapsList = realloc(mapsList, (*mapsCount + 1) * sizeof(procMap_t))) == NULL) {
-+            PLOG_W("realloc failed (sz=%zu)", (*mapsCount + 1) * sizeof(procMap_t));
-+            free(mapsList);
-+            return NULL;
-+        }
-+    }
-+
-+    return mapsList;
-+}
-+
-+static char* arch_searchMaps(unsigned long addr, size_t mapsCnt, procMap_t* mapsList) {
-+    for (size_t i = 0; i < mapsCnt; i++) {
-+        if (addr >= mapsList[i].start && addr <= mapsList[i].end) {
-+            return mapsList[i].name;
-+        }
-+
-+        /* Benefit from maps being sorted by address */
-+        if (addr < mapsList[i].start) {
-+            break;
-+        }
-+    }
-+    return NULL;
-+}
 +
 +/*
 + * Nested loop not most efficient approach, although it's assumed that list is
