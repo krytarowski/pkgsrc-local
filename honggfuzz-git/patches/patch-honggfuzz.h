c714f0a6cb73a5fae61f417e509a8f2992583d84
Kamil Rytarowski <n54@gmx.com>
2018-08-14 01:37:03 +0200

    fix

---
+++ honggfuzz-git/patches/patch-honggfuzz.h
@@ -0,0 +1,44 @@
+$NetBSD$
+
+--- honggfuzz.h.orig	2018-08-13 23:34:18.801995962 +0000
++++ honggfuzz.h
+@@ -298,6 +298,21 @@ typedef struct {
+         bool useClone;
+         sigset_t waitSigSet;
+     } linux;
++    /* For the NetBSD code */
++    struct {
++        void* ignoreAddr;
++        size_t numMajorFrames;
++        pid_t pid;
++        const char* pidFile;
++        char pidCmd[55];
++        const char* symsBlFile;
++        char** symsBl;
++        size_t symsBlCnt;
++        const char* symsWlFile;
++        char** symsWl;
++        size_t symsWlCnt;
++        sigset_t waitSigSet;
++    } netbsd;
+ } honggfuzz_t;
+ 
+ typedef struct {
+@@ -339,6 +354,17 @@ typedef struct {
+         int cpuIptBtsFd;
+     } linux;
+ 
++    struct {
++        /* For NetBSD code */
++        uint8_t* perfMmapBuf;
++        uint8_t* perfMmapAux;
++        hwcnt_t hwCnts;
++        pid_t attachedPid;
++        int cpuInstrFd;
++        int cpuBranchFd;
++        int cpuIptBtsFd;
++    } netbsd;
++
+     bool hasCrashed;
+ } run_t;
+ 
