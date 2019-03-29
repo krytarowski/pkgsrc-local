8a66015169afddc1876b6ec0a1112817d79bf5b4
Kamil Rytarowski <n54@gmx.com>
2019-03-29 12:56:01 +0100

    gix

--- valgrind/patches/patch-coregrind_m__syswrap_priv__types__n__macros.h
+++ valgrind/patches/patch-coregrind_m__syswrap_priv__types__n__macros.h
@@ -2,7 +2,17 @@ $NetBSD$
 
 --- coregrind/m_syswrap/priv_types_n_macros.h.orig	2018-05-05 07:42:22.000000000 +0000
 +++ coregrind/m_syswrap/priv_types_n_macros.h
-@@ -102,6 +102,15 @@ typedef
+@@ -52,6 +52,9 @@
+ /* Arguments for a syscall. */
+ typedef
+    struct SyscallArgs {
++#if defined(VGO_netbsd)
++      Word klass;
++#endif
+       Word sysno;
+       RegWord arg1;
+       RegWord arg2;
+@@ -102,6 +105,15 @@ typedef
        Int o_arg6;
        Int uu_arg7;
        Int uu_arg8;
@@ -18,7 +28,7 @@ $NetBSD$
  #     elif defined(VGP_mips32_linux)
        Int o_arg1;
        Int o_arg2;
-@@ -185,7 +194,7 @@ typedef
+@@ -185,7 +197,7 @@ typedef
  extern
  SyscallTableEntry* ML_(get_linux_syscall_entry)( UInt sysno );
  
@@ -27,7 +37,7 @@ $NetBSD$
  /* XXX: Darwin still uses the old scheme of exposing the table
     array(s) and size(s) directly to syswrap-main.c.  This should be
     fixed. */
-@@ -275,7 +284,7 @@ SyscallTableEntry* ML_(get_solaris_sysca
+@@ -275,7 +287,7 @@ SyscallTableEntry* ML_(get_solaris_sysca
      vgSysWrap_##auxstr##_##name##_after
  
  /* Add a generic wrapper to a syscall table. */
@@ -36,7 +46,7 @@ $NetBSD$
  #  define GENX_(sysno, name)  WRAPPER_ENTRY_X_(generic, sysno, name)
  #  define GENXY(sysno, name)  WRAPPER_ENTRY_XY(generic, sysno, name)
  #elif defined(VGO_darwin)
-@@ -290,6 +299,10 @@ SyscallTableEntry* ML_(get_solaris_sysca
+@@ -290,6 +302,10 @@ SyscallTableEntry* ML_(get_solaris_sysca
  #define LINX_(sysno, name)    WRAPPER_ENTRY_X_(linux, sysno, name) 
  #define LINXY(sysno, name)    WRAPPER_ENTRY_XY(linux, sysno, name)
  
@@ -47,7 +57,7 @@ $NetBSD$
  
  /* ---------------------------------------------------------------------
     Macros useful for writing wrappers concisely.  These refer to the
-@@ -335,7 +348,7 @@ static inline UWord getRES ( SyscallStat
+@@ -335,7 +351,7 @@ static inline UWord getRES ( SyscallStat
     return sr_Res(st->sres);
  }
  
@@ -56,7 +66,7 @@ $NetBSD$
  static inline UWord getRESHI ( SyscallStatus* st ) {
     vg_assert(st->what == SsComplete);
     vg_assert(!sr_isError(st->sres));
-@@ -356,6 +369,13 @@ static inline UWord getERR ( SyscallStat
+@@ -356,6 +372,13 @@ static inline UWord getERR ( SyscallStat
          status->sres = VG_(mk_SysRes_Success)(zzz);  \
     } while (0)
  
@@ -70,7 +80,7 @@ $NetBSD$
  #define SET_STATUS_Failure(zzz)                      \
     do { Word wzz = (Word)(zzz);                      \
          /* Catch out wildly bogus error values. */   \
-@@ -419,7 +439,7 @@ static inline UWord getERR ( SyscallStat
+@@ -419,7 +442,7 @@ static inline UWord getERR ( SyscallStat
  #  define PRA7(s,t,a) PSRAn(7,s,t,a)
  #  define PRA8(s,t,a) PSRAn(8,s,t,a)
  
