ab68bc02ad5b03436d3d87ea928156fccb5e5ac1
Kamil Rytarowski <n54@gmx.com>
2019-06-14 02:44:32 +0200

    fix

--- edb-debugger-git/patches/patch-plugins_DebuggerCore_unix_netbsd_PlatformCommon.cpp
+++
@@ -1,7 +0,0 @@
-$NetBSD$
-
---- plugins/DebuggerCore/unix/netbsd/PlatformCommon.cpp.orig	2019-06-14 00:28:36.190014412 +0000
-+++ plugins/DebuggerCore/unix/netbsd/PlatformCommon.cpp
-@@ -0,0 +1,2 @@
-+
-+#include "PlatformCommon.h"
