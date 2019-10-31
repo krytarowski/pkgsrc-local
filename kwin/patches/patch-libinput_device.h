4695c6ec74983443008d856bab20c2471e4cb0f8
Kamil Rytarowski <n54@gmx.com>
2019-10-31 14:45:29 +0100

    fix

--- kwin/patches/patch-libinput_device.h
+++ kwin/patches/patch-libinput_device.h
@@ -2,12 +2,16 @@ $NetBSD$
 
 --- libinput/device.h.orig	2019-07-30 10:25:09.000000000 +0000
 +++ libinput/device.h
-@@ -20,7 +20,7 @@ along with this program.  If not, see <h
+@@ -20,7 +20,12 @@ along with this program.  If not, see <h
  #ifndef KWIN_LIBINPUT_DEVICE_H
  #define KWIN_LIBINPUT_DEVICE_H
  
--#include <libinput.h>
-+//#include <libinput.h>
++#if defined(__NetBSD__)
++#include <dev/wscons/wsconsio.h>
++#include <sys/ioctl.h>
++#else
+ #include <libinput.h>
++#endif
  
  #include <KConfigGroup>
  
