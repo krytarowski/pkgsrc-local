3f10dd9823b20da061c055e9fa79505d73ba836d
Kamil Rytarowski <n54@gmx.com>
2019-10-31 14:39:32 +0100

    fix

---
+++ kwin/patches/patch-libinput_device.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- libinput/device.h.orig	2019-07-30 10:25:09.000000000 +0000
++++ libinput/device.h
+@@ -20,7 +20,7 @@ along with this program.  If not, see <h
+ #ifndef KWIN_LIBINPUT_DEVICE_H
+ #define KWIN_LIBINPUT_DEVICE_H
+ 
+-#include <libinput.h>
++//#include <libinput.h>
+ 
+ #include <KConfigGroup>
+ 
---
+++ kwin/patches/patch-pointer__input.cpp
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- pointer_input.cpp.orig	2019-07-30 10:25:09.000000000 +0000
++++ pointer_input.cpp
+@@ -53,7 +53,7 @@ along with this program.  If not, see <h
+ // Wayland
+ #include <wayland-cursor.h>
+ 
+-#include <linux/input.h>
++//#include <linux/input.h>
+ 
+ namespace KWin
+ {
